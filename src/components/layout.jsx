e530dfaf747bbbd4d841b44ad7e601acdea45e3e
Daniel Muñoz <djmv225@gmail.com>
2020-12-29 20:51:06 -0500

    Removing commented out code in layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
-// import { rhythm, scale } from '../utils/typography'
 
 class Layout extends React.Component {
   render() {
